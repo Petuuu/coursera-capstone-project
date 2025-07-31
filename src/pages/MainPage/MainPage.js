b4feb3c3608069576b2d4156144d4bd4424fb741
Petu <pojp07@gmail.com>
2025-07-31 16:06:49 +0300

    refactor(App): update file structure

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import Hero from './ui/Hero/Hero';
+import Specials from './ui/Specials/Specials';
+
+function MainPage() {
+  return (
+    <>
+      <Hero />
+      <Specials />
+    </>
+  );
+}
+
+export default MainPage;
