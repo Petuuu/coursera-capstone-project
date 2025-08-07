67889e90df4702ee7f652ad90fefed5fd2da27e5
Petu <pojp07@gmail.com>
2025-08-07 12:44:10 +0300

    refactor(Footer): make bigger array to map over all links

--- src/shared/ui/Footer/Footer.js
+++ src/shared/ui/Footer/Footer.js
@@ -1,26 +1,38 @@
 import './Footer.css';
 
-const navLinks = [
-    { href: '#header', label: 'Home' },
-    { href: '#about', label: 'About' },
-    { href: '#menu', label: 'Menu' },
-    { href: '#reservations', label: 'Reservations' },
-    { href: '#order', label: 'Order Online' },
-    { href: '#login', label: 'Login' }
-]
-
-const contactLinks = [
-    { href: '', label: 'Address' },
-    { href: '', label: 'Phone number' },
-    { href: '', label: 'Email' }
-]
-
-const socialLinks = [
-    { href: '', label: 'LinkedIn' },
-    { href: '', label: 'Instagram' },
-    { href: '', label: 'Facebook' },
-    { href: '', label: 'Twitter' }
-]
+const footerLinks = [
+    {
+        className: "footer__nav",
+        title: "Navigation",
+        links: [
+            { href: "#header", label: "Home" },
+            { href: "#about", label: "About" },
+            { href: "#menu", label: "Menu" },
+            { href: "#reservations", label: "Reservations" },
+            { href: "#order", label: "Order Online" },
+            { href: "#login", label: "Login" }
+        ]
+    },
+    {
+        className: "footer__contact",
+        title: "Contact",
+        links: [
+            { href: "", label: "Address" },
+            { href: "", label: "Phone number" },
+            { href: "", label: "Email" }
+        ]
+    },
+    {
+        className: "footer__social",
+        title: "Social",
+        links: [
+            { href: "", label: "LinkedIn" },
+            { href: "", label: "Instagram" },
+            { href: "", label: "Facebook" },
+            { href: "", label: "Twitter" }
+        ]
+    }
+];
 
 function Footer() {
     return (
@@ -28,32 +40,18 @@ function Footer() {
             <img src="footer-logo.png" alt="Logo" />
 
             <section className="footer__links">
-                <section className="footer__nav">
-                    <h3> Navigation </h3>
-
-                    {navLinks.map(({ href, label }) => (
-                        <a href={href}> {label} </a>
-                    ))}
-                </section>
-
-                <section className="footer__contact">
-                    <h3> Contact </h3>
-
-                    {contactLinks.map(({ href, label }) => (
-                        <a href={href}> {label} </a>
-                    ))}
-                </section>
-
-                <section className="footer__social">
-                    <h3> Social </h3>
-
-                    {socialLinks.map(({ href, label }) => (
-                        <a href={href}> {label} </a>
-                    ))}
-                </section>
+                {footerLinks.map(({ className, title, links }) => (
+                    <section className={className}>
+                        <h3> {title} </h3>
+                        
+                        {links.map(({ href, label }) => (
+                            <a href={href}> {label} </a>
+                        ))}
+                    </section>
+                ))}
             </section>
         </footer>
-    )
+    );
 }
 
 export default Footer;
