ab5a899e1808c8ee5d34f70b205abd09e39a28ea
Petu <pojp07@gmail.com>
2025-08-08 13:41:21 +0300

    feat(ConfirmationPage): add structure and styling for confirmation page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import 'antd/dist/reset.css';
 import './App.css';
 import Header from './shared/ui/Header/Header';
-import ConfirmationPage from './pages/ConfirmationPage/ConfirmationPage';
+import MainPage from './pages/MainPage/MainPage';
 import Footer from './shared/ui/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
-      <ConfirmationPage />
+      <MainPage />
       <Footer />
     </>
   );
